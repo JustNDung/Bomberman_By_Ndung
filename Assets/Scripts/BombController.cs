48ba0291093fec11a574905caca831f0edcf2cf5
JustNDung <nguyenngocdung19122005@gmail.com>
2024-12-14 21:38:28 +0700

    Add explosion effect

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class BombController : MonoBehaviour
 {
+    [Header("Bomb")]
     public GameObject bombPrefab;
     public KeyCode inputKey = KeyCode.Space;
     public float bombFuseTime = 3f;
     public int bombAmount = 1;
     private int bombsRemaining;
+
+    [Header("Explosion")]
+    public Explosion explosionPrefab;
+    public float explosionDuration = 1f;
+    public int explosionRadius = 1;
+    public LayerMask explorationMask;
 
     private void OnEnable()
     {
@@ -34,8 +41,43 @@ public class BombController : MonoBehaviour
 
         yield return new WaitForSeconds(bombFuseTime);
         
+        position = bomb.transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+        
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start);
+        explosion.DestroyAfter(explosionDuration);
+        
+        Explode(position, Vector2.up, explosionRadius);
+        Explode(position, Vector2.down, explosionRadius);
+        Explode(position, Vector2.left, explosionRadius);
+        Explode(position, Vector2.right, explosionRadius);
+        
         Destroy(bomb);
         bombsRemaining++;
+    }
+
+    private void Explode(Vector2 position, Vector2 direction, int length)
+    {
+        if (length <= 0)
+        {
+            return;
+        }
+
+        position += direction;
+
+        if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explorationMask))
+        {
+            return;
+        }
+        
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(length > 1 ? explosion.middle : explosion.end);
+        explosion.SetDirection(direction);
+        explosion.DestroyAfter(explosionDuration);
+
+        Explode(position, direction, length - 1);
     }
 
     private void OnTriggerExit2D(Collider2D other)
---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public AnimatedSpriteRenderer start;
+    public AnimatedSpriteRenderer middle;
+    public AnimatedSpriteRenderer end;
+
+    public void SetActiveRenderer(AnimatedSpriteRenderer renderer)
+    {
+        start.enabled = renderer == start;
+        middle.enabled = renderer == middle;
+        end.enabled = renderer == end;
+    }
+
+    public void SetDirection(Vector2 dir)
+    {
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        // Rotate some angle around some axis 
+    }
+
+    public void DestroyAfter(float seconds)
+    {
+        Destroy(gameObject, seconds);
+    }
+}
